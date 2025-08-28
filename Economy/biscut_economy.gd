f0e2e44787352f84ad127f8c32de1efb341a5679
ClowFsh <naholloway@yahoo.com>
2025-08-28 04:02:36 -0400

    did some edits

--- Economy/biscut_economy.gd
+++ Economy/biscut_economy.gd
@@ -6,7 +6,9 @@ enum Type {
 	Ice,
 	Earth,
 }
-# Biscut Types
 
-func generate_enemy_loot(defeted_enemy):
+func _ready() -> void:
+	EventBus.ENEMY.defeted.connect(generate_enemy_loot_for_player)
+
+func generate_enemy_loot_for_player(defeted_enemy):
 	pass
--- player.gd
+++ player.gd
@@ -2,4 +2,5 @@ extends Node
 # the disembodied player character
 
 @export var health := 20
-@export var biscuts:Dictionary[BiscutEconomy.Type,int] = {}
+@export var biscuts := 10
+@export var power_buiscuts:Dictionary[BiscutEconomy.Type,int] = {}
